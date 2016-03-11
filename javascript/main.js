4ff955d8cdc2a5c202a7e9f649a123fb8df21d2f
DakotaHarward <daktoa101@gmail.com>
2016-03-10 19:58:27 -0700

    Did more stuff.

---
+++ javascript/main.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("commercialContent").style.display = 'none';
+    document.getElementById("educationalContent").style.display = 'none';
+    document.getElementById("researchContent").style.display = 'none';
+    document.getElementById("aboutUASContent").style.display = 'initial';
+});
+function about() {
+    document.getElementById("aboutUASli").style.backgroundColor = "#111";
+    document.getElementById("aboutUASa").style.backgroundColor = "#111";
+    document.getElementById("commercialli").style.backgroundColor = "#333";
+    document.getElementById("commerciala").style.backgroundColor = "#333";
+    document.getElementById("educationalli").style.backgroundColor = "#333";
+    document.getElementById("educationala").style.backgroundColor = "#333";
+    document.getElementById("researchli").style.backgroundColor = "#333";
+    document.getElementById("researcha").style.backgroundColor = "#333";
+    document.getElementById("commercialContent").style.display = 'none';
+    document.getElementById("educationalContent").style.display = 'none';
+    document.getElementById("researchContent").style.display = 'none';
+    document.getElementById("aboutUASContent").style.display = 'initial';
+}
+function commercial() {
+    document.getElementById("commercialli").style.backgroundColor = "#111";
+    document.getElementById("commerciala").style.backgroundColor = "#111";
+    document.getElementById("aboutUASli").style.backgroundColor = "#333";
+    document.getElementById("aboutUASa").style.backgroundColor = "#333";
+    document.getElementById("educationalli").style.backgroundColor = "#333";
+    document.getElementById("educationala").style.backgroundColor = "#333";
+    document.getElementById("researchli").style.backgroundColor = "#333";
+    document.getElementById("researcha").style.backgroundColor = "#333";
+    document.getElementById("educationalContent").style.display = 'none';
+    document.getElementById("researchContent").style.display = 'none';
+    document.getElementById("aboutUASContent").style.display = 'none';
+    document.getElementById("commercialContent").style.display = 'initial';
+}
+function educational() {
+    document.getElementById("educationalli").style.backgroundColor = "#111";
+    document.getElementById("educationala").style.backgroundColor = "#111";
+    document.getElementById("commercialli").style.backgroundColor = "#333";
+    document.getElementById("commerciala").style.backgroundColor = "#333";
+    document.getElementById("aboutUASli").style.backgroundColor = "#333";
+    document.getElementById("aboutUASa").style.backgroundColor = "#333";
+    document.getElementById("researchli").style.backgroundColor = "#333";
+    document.getElementById("researcha").style.backgroundColor = "#333";
+    document.getElementById("aboutUASContent").style.display = 'none';
+    document.getElementById("commercialContent").style.display = 'none';
+    document.getElementById("researchContent").style.display = 'none';
+    document.getElementById("educationalContent").style.display = 'initial';
+}
+function research() {
+    document.getElementById("researchli").style.backgroundColor = "#111";
+    document.getElementById("researcha").style.backgroundColor = "#111";
+    document.getElementById("commercialli").style.backgroundColor = "#333";
+    document.getElementById("commerciala").style.backgroundColor = "#333";
+    document.getElementById("educationalli").style.backgroundColor = "#333";
+    document.getElementById("educationala").style.backgroundColor = "#333";
+    document.getElementById("aboutUASli").style.backgroundColor = "#333";
+    document.getElementById("aboutUASa").style.backgroundColor = "#333";
+    document.getElementById("aboutUASContent").style.display = 'none';
+    document.getElementById("commercialContent").style.display = 'none';
+    document.getElementById("educationalContent").style.display = 'none';
+    document.getElementById("researchContent").style.display = 'initial';
+}
